2f18e35a602c537f68e161fbba7f9f3a56f7e9e3
Evangelos Karachristos <karachristos.e@outlook.com>
2023-03-03 23:25:55 +0200

    Add project to github

---
+++ src/main/java/com/example/attikometroapi/AttikoMetroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.attikometroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttikoMetroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttikoMetroApiApplication.class, args);
+    }
+
+}
